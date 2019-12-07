a3941bbcafc4837649f9a69f673610843bb0c9e6
Povarov <cook0208@mail.ru>
2019-12-07 12:05:35 +0300

    Added searchControl to Yandex Map

--- application/screens/Map.js
+++ application/screens/Map.js
@@ -3,30 +3,49 @@ import {
   StyleSheet,
   View
 } from 'react-native';
-import { YMaps, Map as YMap, Placemark } from 'react-yandex-maps';
+import { YMaps, Map as YMap, Placemark, SearchControl } from 'react-yandex-maps';
 import { connect } from 'react-redux';
+import socket from '../store/socket';
+import { array } from 'prop-types';
 
 const mapData = {
   center: [57.631285, 39.840864],
   zoom: 14,
+  controls: []
 };
 
-const coordinates = [
+const coordinates = [ // заглушка
   [57.631285, 39.840875],
   [57.631260, 39.840864]
 ];
 
+const API_KEY = "0a2d7a02-6fe0-4f2b-b87b-c6afd3e44764";
+
 class Map extends React.Component {
   constructor(props) {
     super(props);
+    socket.emit('findTeammates', {sender: this.props.user}); // запрос на получение координат участников игры
+  }
+
+  state = { 
+    teammates: [] 
+    // можно добавить имена
+  }
+
+  componentDidMount() { // событие, возвращающее координаты игроков
+    var self = this;
+    socket.on('nearTeammates', function (teammates) {
+      self.setState(teammates)
+    });
   }
 
   render() {
-    return (
+    return ( // state.teammates вместо coordinates (done)
       <View style={styles.container}>
-        <YMaps>
-          <YMap style={StyleSheet.absoluteFillObject} defaultState={mapData}>
-            {coordinates.map(coordinate => <Placemark properties={{balloonContent: 'Some text'}} geometry={coordinate} />)}
+        <YMaps enterprise query = {{apikey: API_KEY}}>
+          <YMap style={StyleSheet.absoluteFillObject} defaultState={mapData}> 
+            <SearchControl options={{ float: 'right', provider: 'yandex#map', noPlacemark: false }} />
+            {this.state.teammates.map(coordinate => <Placemark properties={{balloonContent: 'Some text'}} geometry={coordinate} />)}
           </YMap>
         </YMaps>
       </View>
