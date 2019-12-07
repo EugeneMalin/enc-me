f808f5c7539741a55675d56cab13e3d896a63ef8
Eugene Malin(Novikov) <jenia96@live.com>
2019-12-07 22:08:01 +0300

    Correct header

--- application/components/Header.js
+++ application/components/Header.js
@@ -4,7 +4,7 @@ import { Drawer, withTheme, Text } from 'react-native-paper';
 
 function Header(props) {
   return (
-    <Drawer.Section style={{ height: '100%', backgroundColor: props.theme['colors']['background'] }}>
+    <Drawer.Section style={{ height: '100%', width: '100%', backgroundColor: props.theme['colors']['background'] }}>
         <Text>{props.user && props.user.gameId ? props.title : 'No active games'}</Text>
         <Text>{props.user && props.user.gameId ? props.subtitle : ''}</Text>
     </Drawer.Section>
