8d5624ac597a0c94739b63a15afe81e10a3f49e8
VladimirSinitsin <58597891+VladimirSinitsin@users.noreply.github.com>
2019-12-07 15:12:03 +0300

    Update relations.ts

--- service/lib/relations.ts
+++ service/lib/relations.ts
@@ -2,6 +2,11 @@ import User from './model/user'
 import Message from './model/message'
 import Answer from './model/answer'
 
+User.hasMany(Message);
+User.hasMany(Answer);
+Message.belongsTo(User);
+Answer.belongsTo(User);
+
 export {
     User,
     Message,
