fe41d568dfb4c2109cfda79b9b6f4fd0758b937f
Mefrit <mihail_fokin_1999@mail.ru>
2019-12-07 09:16:18 +0300

    service/M_Fokin/answer table created

--- service/lib/model/answer.ts
+++ service/lib/model/answer.ts
@@ -1,6 +1,24 @@
-import {connection} from "../sequelize";
-import { Model, DataTypes } from 'sequelize';  
+import { connection } from "../sequelize";
+import { Model, DataTypes } from 'sequelize';
 
-class Answer extends Model {}
+class Answer extends Model {
+    public id!: number;
+    public userAnswer!: number | string;
+    public taskId!: number | string;
+    public validAnswer!: boolean;
+}
 
+
+Answer.init({
+    idAnswer: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userAnswer: DataTypes.TEXT,
+    taskId: DataTypes.STRING,
+    validAnswer: DataTypes.BOOLEAN
+}, {
+    modelName: 'answer', sequelize: connection
+})
 export default Answer
