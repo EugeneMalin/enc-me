3967ae79bec55eca91d6123f1094093fe482cddb
VladimirSinitsin <58597891+VladimirSinitsin@users.noreply.github.com>
2019-12-07 15:12:00 +0300

    Update message.ts

--- service/lib/model/message.ts
+++ service/lib/model/message.ts
@@ -1,6 +1,28 @@
-import { Model, DataTypes } from 'sequelize';  
-import {connection} from '../sequelize';
+  
+import { Model, DataTypes, UUIDV1 } from "sequelize";
+import {connection} from "../sequelize";
 
-class Message extends Model {}
+class Message extends Model {
+    public id!: number;
+    public user_id!: number;
+    public message!: number|string;
+    public creatAt!: Date;
+}
 
-export default Message;
+    Message.init({
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: UUIDV1
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+        },
+        message:{ 
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+    }, {
+        modelName: 'message', sequelize: connection
+    })
+export default Message
