dd2df0cf0b1fbf79f28ee982357f6820068907c1
Mefrit <mihail_fokin_1999@mail.ru>
2019-12-07 09:15:42 +0300

    service/M_Fokin/answer table created

--- service/app.ts
+++ service/app.ts
@@ -7,35 +7,6 @@ import { connection, IMobileSockets } from './lib/sequelize'
 import './lib/relations';
 
 
-import { request } from 'http';
-
-
-export function post(data: object, path: string) {
-    return new Promise<string>((resolve) => {
-        const req = request({
-            hostname: 'http://10.76.173.216',
-            port: 5055,
-            path: path,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }, (res) => {
-            res.setEncoding('utf8');
-            res.on('data', (body) => {
-                console.log('body', body);
-                resolve(body);
-            });
-        })
-        req.on('error', function (e) {
-            console.log('problem with request: ' + e.message);
-        });
-        req.write(JSON.stringify(data))
-        req.end()
-    })
-}
-//зов функции  post
-setInterval(() => { post({ 'accountName': 'novikov', 'password': '12345678' }, '/api/account/signIn') }, 1000);
 
 
 const io: Server = socket(http.createServer().listen(1387));
@@ -54,66 +25,3 @@ connection.sync().then(() => {
         })
     })
 })
-// var url = 'http://10.76.173.216:5055/showAllGames/';
-
-
-
-
-
-
-// var options_v2 =
-// {
-//     hostname: '10.76.173.216',
-//     port: 5055,
-
-//     method: 'POST',
-//     body: bodyObj,
-//     headers: {
-//         'Content-Type': 'application/json',
-//     }
-
-// };
-
-// var bodyObj = JSON.stringify({ 'accountName': 'novicow', 'password': '12345678' });
-// var options =
-// {
-//     hostname: '10.76.173.216',
-//     port: 5055,
-//     path: "/api/account/signIn",
-//     method: 'POST',
-//     body: bodyObj,
-//     headers: {
-//         'Content-Type': 'application/json',
-//     }
-
-// };
-// http.request(options, (data) => {
-//     console.log('answer -- > ', data);
-// });
-// setInterval(() => {
-//     http.request(options_v2, (data) => {
-//         console.log('answer -- > ', data);
-//     });
-
-// }, 3000);
-// // http.request(options, (data) => {
-// //     console.log('answer --> ', data);
-// // });
-
-
-
-
-// var options =
-// {
-//     hostname: '10.76.173.216',
-//     port: 5055,
-//     path: "",
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     }
-
-// };
-// http.request(options, (data) => {
-//     console.log('answer ----> ', data);
-// });
